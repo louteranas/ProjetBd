5da242a6d22461eff50bb1da4ff75e3873658535
Ananas <louteranas@gmail.com>
2018-12-05 10:01:15 +0100

    coucou mon pc est mort

---
+++ src/Graphics/ajoutSalle.java
@@ -0,0 +1,44 @@
+package Graphics;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class ajoutSalle {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ajoutSalle window = new ajoutSalle();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ajoutSalle() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+	}
+
+}
