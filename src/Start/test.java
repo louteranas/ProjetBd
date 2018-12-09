84faff1526415f3a2c2d87cdb00c4d348eb07a22
leila160 <leila.kany@gmail.com>
2018-12-09 19:48:20 +0100

    correction de durée

--- src/Start/test.java
+++ src/Start/test.java
@@ -13,33 +13,33 @@ public class test {
     /**
      * Launch the application.
      */
+//    public static void main(String[] args) {
+//        DataBaseAccess data = new DataBaseAccess();
+//        Vente act = new Vente("leila.kany@gmail.com", data);
+//        ArrayList<String>  car= new ArrayList<>();
+//        car.add("tres beau");
+//
+//        try {
+//            act.newVente(car, 100, 5, "velo", 30, 3, 3);
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//
+//        }
+//    }
+//}
     public static void main(String[] args) {
-        DataBaseAccess data = new DataBaseAccess();
-        Vente act = new Vente("leila.kany@gmail.com", data);
-        ArrayList<String>  car= new ArrayList<>();
-        car.add("tres beau");
-
-        try {
-            act.newVente(car, 100, 5, "velo", 30, 3, 3);
-        } catch (SQLException e) {
-            e.printStackTrace();
-
-        }
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    login window = new login();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
-
-//    public static void main(String[] args) {
-//        EventQueue.invokeLater(new Runnable() {
-//            public void run() {
-//                try {
-//                    login window = new login();
-//                    window.frame.setVisible(true);
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
-//    }
 
 
 
